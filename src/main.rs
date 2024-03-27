96c29f23539972e0b999663b904b3620bc513cb2
Shayegan8 <shayeganrood85@gmail.com>
2024-03-28 00:38:23 +0330

    fixed errors

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,22 @@ use std::env::ArgsOs;
 use std::io::{BufRead, BufReader, Write};
 
 fn handle_conn(mut stream: TcpStream) {
-    let reader:BufReader<&mut TcpStream> = BufReader::new(&mut stream);
-    let req: Vec<_> = reader.lines().map(|x| x.unwrap())
-        .take_while(|line| !line.is_empty()).collect();
 
-    let req_line = req.lines().next().unwrap().unwrap();
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line:String = buf_reader.lines().next().unwrap().unwrap();
 
-    if req_line == "GET / HTTP/1.1" {
+    if request_line == "GET / HTTP/1.1" {
         let status = "HTTP/1.1 200 OK";
         fs::read_dir("htmls");
-        for file in fs::read_dir("htmls") {
-            let file = file?;
-            let path = file.path();
-            if path.is_file() {
-                let contents = fs::read_to_string(path).unwrap();
-                let length = contents.len();
-                let response = "{status}\r\nContent-Length: {length}\r\n\r\n{contents}";
-                stream.write_all(response.as_bytes()).unwrap();
+        if let Ok(files) = fs::read_dir("htmls") {
+            for file in files {
+                if file.is_ok() {
+                let path = file.unwrap().file_name();
+                    let contents = fs::read_to_string(path).unwrap();
+                    let length = contents.len();
+                    let response = "{status}\r\nContent-Length: {length}\r\n\r\n{contents}";
+                    stream.write_all(response.as_bytes()).unwrap();
+                }
             }
         }
     } else {
@@ -33,10 +32,10 @@ fn handle_conn(mut stream: TcpStream) {
         stream.write_all(response.as_bytes()).unwrap();
     }
 
-    println!("REQ: {:#?}", req);
+    println!("REQ: {:#?}", request_line);
 }
 
-fn main() -> u32 {
+fn main() {
 
     let args: ArgsOs = env::args_os();
     let bind:TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -47,6 +46,4 @@ fn main() -> u32 {
         let stream: TcpStream = stream.unwrap();
         handle_conn(stream);
     }
-
-    0
 }
