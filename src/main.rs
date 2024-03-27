c39f24e350083e365e55a02f743fe28526095d32
Shayegan8 <shayeganrood85@gmail.com>
2024-03-28 00:54:14 +0330

    error fixed

--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,40 @@ use std::{env, fs};
 use std::env::ArgsOs;
 use std::io::{BufRead, BufReader, Write};
 
-fn handle_conn(mut stream: TcpStream) {
-
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line:String = buf_reader.lines().next().unwrap().unwrap();
+fn handle_conn(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let mut buf_reader = BufReader::new(&mut stream);
+    let request_line = match buf_reader.lines().next() {
+        Some(Ok(line)) => line,
+        _ => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Invalid request line")),
+    };
 
     if request_line == "GET / HTTP/1.1" {
         let status = "HTTP/1.1 200 OK";
-        fs::read_dir("htmls");
-        if let Ok(files) = fs::read_dir("htmls") {
-            for file in files {
-                if file.is_ok() {
-                let path = file.unwrap().file_name();
-                    let contents = fs::read_to_string(path).unwrap();
-                    let length = contents.len();
-                    let response = "{status}\r\nContent-Length: {length}\r\n\r\n{contents}";
-                    stream.write_all(response.as_bytes()).unwrap();
-                }
+        let files = fs::read_dir("htmls").expect("Error reading directory");
+        for file in files {
+            if let Ok(file) = file {
+                let path = file.file_name();
+                let contents = fs::read_to_string(path).expect("Error reading file contents");
+                let length = contents.len();
+                let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}", status = status, length = length, contents = contents);
+                stream.write_all(response.as_bytes())?;
             }
         }
     } else {
         let status = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("htmls/404.html");
-        let response = "{status}\r\nContent-Length: {length}\r\n\r\n{contents}";
-
-        stream.write_all(response.as_bytes()).unwrap();
+        let contents = fs::read_to_string("htmls/404.html").expect("Error reading 404 file");
+        let length = contents.len();
+        let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}", status = status, length = length, contents = contents);
+        stream.write_all(response.as_bytes())?;
     }
 
     println!("REQ: {:#?}", request_line);
+    Ok(())
 }
-
 fn main() {
 
     let args: ArgsOs = env::args_os();
-    let bind:TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let bind:TcpListener = TcpListener::bind("127.0.0.1:21039").unwrap();
 
     println!("Listening on clients");
 
