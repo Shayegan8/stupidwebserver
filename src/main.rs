27385fdc3a7b50977c50f43c122bf5bd7dcc9a0f
Shayegan8 <shayeganrood85@gmail.com>
2024-04-03 13:01:55 +0330

    code refactored

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-#![allow(unused)]
-
 use local_ip_address::local_ip;
-use std::env::Args;
-use std::error::Error;
 use std::ffi::OsString;
-use std::fmt::Debug;
 use std::io::Error as Er;
-use std::io::ErrorKind as Kind;
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
-use std::path;
 use std::path::PathBuf;
 use std::process::exit;
-use std::{env, fmt, fs};
+use std::{env, fs};
 
 fn handle_conn(
     mut stream: TcpStream,
     vec: &Vec<OsString>,
-    mut address: &String,
 ) -> Result<(), std::io::Error> {
     let mut buf_reader = BufReader::new(&mut stream);
     let mut request_line = String::new();
@@ -44,27 +36,26 @@ fn handle_conn(
         } else {
             continue;
         }
-
-        if (is_writted == false) {
-            println!("writing 404");
-            let status = "HTTP/1.1 404 NOT FOUND";
-            let contents = fs::read_to_string("htmls/404.html")?;
-            let length = contents.len();
-            let response = format!(
-                "{status}\r\nContent-Length: {length}\r\n\r\n{contents}",
-                status = status,
-                length = length,
-                contents = contents
-            );
-            stream.write_all(response.as_bytes())?;
-        }
+    }
+    if is_writted == false {
+        println!("writing 404");
+        let status = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("htmls/404.html")?;
+        let length = contents.len();
+        let response = format!(
+            "{status}\r\nContent-Length: {length}\r\n\r\n{contents}",
+            status = status,
+            length = length,
+            contents = contents
+        );
+        stream.write_all(response.as_bytes())?;
     }
 
     println!("REQ: {:#?}", request_line);
     Ok(())
 }
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     let mut vec: Vec<OsString> = vec![];
     let path = PathBuf::from("htmls");
     for string in path.read_dir().unwrap() {
@@ -88,14 +79,14 @@ fn main() {
     let bind: Result<TcpListener, Er> = TcpListener::bind(&address);
 
     match bind {
-        Ok(ref bind) => println!("Listening on clients"),
-        Err(e) => {
+        Ok(ref _bind) => println!("Listening on clients"),
+        Err(_e) => {
             println!("Port is already in use or not exist");
             exit(1);
         }
     }
 
-    let bind = bind.unwrap_or_else(|msg| {
+    let bind = bind.unwrap_or_else(|_msg| {
         eprintln!("Can't bind listener");
         exit(1);
     });
@@ -103,7 +94,7 @@ fn main() {
     for stream in bind.incoming() {
         match stream {
             Ok(stream) => {
-                handle_conn(stream, &vec, &address).unwrap();
+                handle_conn(stream, &vec).unwrap();
             }
             Err(err) => {
                 eprint!("{}", err);
